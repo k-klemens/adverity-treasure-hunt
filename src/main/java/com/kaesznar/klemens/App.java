6977b466eb25f85fb8bfb840c0d22d93ab585eba
Klemens Kaesznar <e11776832@tuwien.ac.at>
2020-01-31 08:50:07 +0100

    inital import

---
+++ src/main/java/com/kaesznar/klemens/App.java
@@ -0,0 +1,13 @@
+package com.kaesznar.klemens;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
